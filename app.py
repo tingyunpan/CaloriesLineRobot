5183d792e536902675bbe889a99acdf30940901a
tingyunpan <111957786+tingyunpan@users.noreply.github.com>
2022-08-25 08:21:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,5 +45,7 @@ def handle_message(event):
 #主程式
 import os 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    from wsgiref.simple_server import make_server
+    server = make_server('0.0.0.0',5000,app)
+    Server.serve_forever()
+    app.run()
